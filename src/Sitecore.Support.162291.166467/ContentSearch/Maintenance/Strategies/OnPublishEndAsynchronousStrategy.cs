570fd29505617512803d9c4163eb7aa6b0954776
AFI <SC-AFI@users.noreply.github.com>
2017-06-07 16:07:42 +0300

    Empty commit

---
+++ src/Sitecore.Support.162291.166467/ContentSearch/Maintenance/Strategies/OnPublishEndAsynchronousStrategy.cs
@@ -0,0 +1,19 @@
+﻿namespace Sitecore.Support.ContentSearch.Maintenance.Strategies
+{
+  using Data.Archiving;
+  using Jobs;
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.Diagnostics;
+  using Sitecore.ContentSearch.Maintenance;
+  using Sitecore.Data;
+  using Sitecore.Data.Eventing.Remote;
+  using Sitecore.Eventing;
+  using Sitecore.Globalization;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  public class OnPublishEndAsynchronousStrategy : Sitecore.ContentSearch.Maintenance.Strategies.OnPublishEndAsynchronousStrategy
+  {
+
+  }
+}
---
+++ src/Sitecore.Support.162291.166467/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.162291.166467")]
+[assembly: AssemblyProduct("Sitecore.Support.162291.166467")]
+[assembly: ComVisible(false)]
